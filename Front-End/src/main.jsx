5798464792f18b66def208f3c2a878575d073680
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-06 15:27:34 -0300

    Adiciona configuração de roteamento usando react-router-dom
    O arquivo importa os componentes Consultar e Cadastrar para mapear diferentes rotas. Define uma rota padrão para o componente Consultar ("/") e uma rota para o componente Cadastrar ("/cadastrar"). O roteamento é encapsulado dentro de um BrowserRouter e renderizado usando ReactDOM.createRoot.
     Esta configuração permite a navegação entre diferentes componentes com base na URL.

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Consultar from './Components/Consultar/index.jsx'
+import Cadastrar from './Components/Cadastrar/index.jsx'
+
+const router = createBrowserRouter ([
+  {
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <Consultar/>,
+      },
+      {
+        path: "cadastrar",
+        element: <Cadastrar/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
