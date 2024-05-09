de0cac6aac399a1e2ff9d14e1ad7eec8ac590cc6
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-09 20:09:11 -0300

    Atualiza componente App para buscar dados do servidor
    Atualiza o componente App para incluir uma chamada assíncrona à API do servidor para recuperar dados.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,8 +1,29 @@
 import { Outlet } from 'react-router-dom'
 import './styles.css'
 import Navbar from './Components/Navbar'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() => {
+
+    try {
+      const response = await axios.get("http://localhost:8080/listar");
+
+      const data = response.data;
+
+      setPosts(data)
+    }catch (error){
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  },[]);
 
   return (
     <>
