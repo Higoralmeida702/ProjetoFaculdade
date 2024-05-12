4ca9a8afbd49a01c9119b11f38ba405f4703cc51
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-11 22:45:26 -0300

    Atualiza AlunosInfoRepository
    Apenas uma pequena formatação e organização no código do repositório AlunosInfoRepository.

--- src/main/java/com/higor/projetofaculdade/Repositories/AlunosInfoRepository.java
+++ src/main/java/com/higor/projetofaculdade/Repositories/AlunosInfoRepository.java
@@ -3,5 +3,11 @@ package com.higor.projetofaculdade.Repositories;
 import com.higor.projetofaculdade.Entities.AlunoInformacoes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlunosInfoRepository extends JpaRepository <AlunoInformacoes, Integer> {
+
+    Optional<AlunoInformacoes> findByCpf(int cpf);
+
 }
+
