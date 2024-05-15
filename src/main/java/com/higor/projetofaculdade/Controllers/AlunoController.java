98c92eee3051a5f94180cb288bb32af4c653fedf
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-15 18:25:51 -0300

    Atualiza método de alteração de informações dos alunos
    Esta atualização aprimora o método de alteração para permitir a modificação de informações específicas de alunos através de seu CPF de forma mais simplificada.

--- src/main/java/com/higor/projetofaculdade/Controllers/AlunoController.java
+++ src/main/java/com/higor/projetofaculdade/Controllers/AlunoController.java
@@ -1,13 +1,21 @@
 package com.higor.projetofaculdade.Controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import com.higor.projetofaculdade.Entities.AlunoInformacoes;
 import com.higor.projetofaculdade.Entities.RespostaModelo;
 import com.higor.projetofaculdade.Services.AlunoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 
 @RestController
@@ -36,11 +44,6 @@ public class AlunoController {
         return alunoService.remover(id);
     }
 
-    @PutMapping ("/alterar")
-    public ResponseEntity<?> alterar (@RequestBody AlunoInformacoes alunoInformacoes){
-        return alunoService.cadastrarAlterar(alunoInformacoes, "alterar");
-    }
-
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody AlunoInformacoes alunoInformacoes) {
         return alunoService.cadastrarAlterar(alunoInformacoes, "cadastrar");
@@ -56,5 +59,40 @@ public class AlunoController {
         return "API de produtos funcionando";
     }
 
+    @PutMapping("/alterar/{cpf}")
+        public ResponseEntity<?> alterar(@PathVariable int cpf, @RequestBody AlunoInformacoes alunoInformacoes) {
+        try {
+        Optional<AlunoInformacoes> alunoOptional = alunoService.buscarPorCPF(cpf);
+        if (alunoOptional.isPresent()) {
+            AlunoInformacoes aluno = alunoOptional.get();
+            aluno.setOcorrencia(alunoInformacoes.getOcorrencia());
+            aluno.setNome(alunoInformacoes.getNome());
+            aluno.setEstado(alunoInformacoes.getEstado());
+            aluno.setCelular(alunoInformacoes.getCelular());
+            aluno.setCidade(alunoInformacoes.getCidade());
+            aluno.setEstado(alunoInformacoes.getEstado());
+            aluno.setEmail(alunoInformacoes.getEmail());
+            aluno.setRg(alunoInformacoes.getRg());
+            aluno.setCpf(alunoInformacoes.getCpf());
+            aluno.setMatricula(alunoInformacoes.getMatricula());
+            aluno.setOrgaoExpedidor(alunoInformacoes.getOrgaoExpedidor());
+            aluno.setNomePai(alunoInformacoes.getNomePai());
+            aluno.setNomeMae(alunoInformacoes.getNomeMae());
+            aluno.setTurno(alunoInformacoes.getTurno());
+            aluno.setDataNascimento(alunoInformacoes.getDataNascimento());
+            aluno.setBairro(alunoInformacoes.getBairro());
+            aluno.setNacionalidade(alunoInformacoes.getNacionalidade());
+            alunoService.cadastrarAlterar(aluno, "alterar");
+            return ResponseEntity.ok("Informação alterada com sucesso para o aluno de CPF: " + cpf);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro ao alterar informação para o aluno de CPF: " + cpf + ": " + e.getMessage());
+    }
+}
+
 
 }
+
